72314572825141748106b57a3ef7e69e42ab2067
guilhermecarolino35 <67343764+guilhermecarolino35@users.noreply.github.com>
2026-01-17 16:33:02 -0300

    update client

--- client.py
+++ client.py
@@ -3,23 +3,25 @@ import time
 import random
 import requests
 
-class Client: 
+class Client:
     def __init__(self):
         self.client_id = self._load_client_id()
+        self.process_url = self._load_process_url()
 
     def _load_client_id(self):
         client_id = os.getenv("CLIENT_ID")
-
         if client_id is None:
-            raise RuntimeError("Cliente_ID não definido no ambiente")
-        
+            raise RuntimeError("CLIENT_ID não definido no ambiente")
         return int(client_id)
-    
+
+    def _load_process_url(self):
+        process_url = os.getenv("PROCESS_URL")
+        if process_url is None:
+            raise RuntimeError("PROCESS_URL não definido no ambiente")
+        return process_url
 
     def generate_request_timestamp(self):
-        timestamp_ms = int(time.time() * 1000)
-        return (timestamp_ms,self.client_id)
-    
+        return int(time.time() * 1000)
 
     def send_write_request(self):
         payload = {
@@ -33,19 +35,19 @@ class Client:
             timeout=60
         )
 
-        return response.json()
-    
-    def wait_for_commited(self,response):
+        return response
+
+    def wait_for_committed(self, response):
         data = response.json()
-        if data.get("status") != "COMMITED":
-            raise RuntimeError("Resposta invalida do CLuster Sync")
-        
+        if data.get("status") != "COMMITTED":
+            raise RuntimeError("Resposta inválida do Cluster Sync")
+
     def sleep_random(self):
-        sleep_time = random.uniform(1,5)
+        sleep_time = random.uniform(1, 5)
         time.sleep(sleep_time)
 
     def run_loop(self):
-        for i in range(30):
+        for _ in range(30):
             response = self.send_write_request()
-            self.wait_for_commited(response)
+            self.wait_for_committed(response)
             self.sleep_random()
